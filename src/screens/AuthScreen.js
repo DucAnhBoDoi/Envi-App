05df9c34c7e85224e89d55d5a2f3f03271389db6
imsowdt <sowsdnice@gmail.com>
2025-10-30 19:13:08 +0700

    hehe added

--- src/screens/AuthScreen.js
+++ src/screens/AuthScreen.js
@@ -14,6 +14,7 @@ import { Ionicons } from "@expo/vector-icons";
 import { AuthContext } from "../context/AuthContext";
 import { useGoogleAuth } from "../services/oauthService";
 
+//hehe hoang
 // 🔹 Hàm chuyển lỗi Firebase sang tiếng Việt
 const getFirebaseErrorMessage = (errorCode) => {
   switch (errorCode) {
